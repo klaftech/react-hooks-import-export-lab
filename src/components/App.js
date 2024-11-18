523235ac6563fb0bd52a68eccbc09a3595c277ae
Elchonon <ekn400@gmail.com>
2024-11-18 10:23:11 -0500

    lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React from "react";
+import NavBar from "./NavBar.js"
+import Home from "./Home"
+import About from './About';
+import { username, city, image } from '../data/user.js'
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home username={username} city={city} />
+      <About image={image} />
     </div>
   );
 }
